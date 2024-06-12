7bc46ff2064dd53f841e8942e00fcd3514055384
Your Name <hsssseeeen2002@gmail.com>
2024-06-12 10:26:03 +0300

    add tictoc

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Note } from "./Notes/Note";
 import { NoteLayout } from "./Notes/NoteLayout";
 import { EditNote } from "./Notes/EditNote";
 import { v4 as uuidV4 } from 'uuid';
+import TicToc from "./components/TicToc";
 export type Note = {
   id: string;
 } & NoteDate;
@@ -54,6 +55,10 @@ function App() {
     {
       title: 'Task Manager',
       url: 'task-manager'
+    },
+    {
+      title: 'TicToc',
+      url: 'tictoc'
     }
   ]
   const [notes, setNotes] = useLocalStorage<RowNote[]>("NOTES", []);
@@ -101,60 +106,62 @@ function App() {
   return (
 
     <div className="">
-      {open === false && <button className="p-3  absolute top-3 left-3 w-11 hover:bg-slate-800 bg-black rounded flex flex-col gap-y-1" onClick={() => setOpen(true)}><span className="w-full bg-white h-[2px]"></span><span className="w-full bg-white h-[2px]"></span><span className="w-full bg-white h-[2px]"></span></button> }
+      {open === false && <button className="p-3  absolute top-3 left-3 w-11 hover:bg-slate-800 bg-black rounded flex flex-col gap-y-1" onClick={() => setOpen(true)}><span className="w-full bg-white h-[2px]"></span><span className="w-full bg-white h-[2px]"></span><span className="w-full bg-white h-[2px]"></span></button>}
       {open && (
         <div className={`absolute z-10 transition-all duration-500 left-0 top-0 grid ${open ? 'grid-rows-1' : 'grid-rows-[0]'}`}>
           <button className=" absolute flex items-center justify-center top-3 left-36 size-10 hover:bg-slate-800 bg-black rounded text-white text-3xl font-bold" onClick={() => setOpen(false)}>&times;</button>
           <div className="w-[200px] bg-slate-600 flex flex-col gap-y-3  pl-2 h-svh pt-24">
             {links.map((item, id) => {
               return (
-                <Link key={id} to={item.url} className="p-2 w-[180px] rounded bg-slate-800 text-slate-400 hover:text-white" style={{color: isActive === id? 'white': ''}} onClick={()=> setIsActive(id)}>{item.title}</Link>
+                <Link key={id} to={item.url} className="p-2 w-[180px] rounded bg-slate-800 text-slate-400 hover:text-white" style={{ color: isActive === id ? 'white' : '' }} onClick={() => setIsActive(id)}>{item.title}</Link>
               )
             })}
           </div>
         </div>
       )}
+
       <Routes>
-      <Route path='/' element={<SpeedType />} />
-      <Route path='/type-speed-game' element={<SpeedType />} />
-      <Route path='/hangman' element={<MainHangMan />} />
-      <Route path='/todo/*' element={<Todo />} />
-      <Route
-        path='/task-manager'
-        element={
-          <NoteList
-            notes={notesWithTags}
-            availableTags={tags}
-            onUpdateTag={onUpdateTag}
-            onDeleteTag={onDeleteTag}
-          />
-        }
-      />
-      <Route
-        path='/task-manager/new'
-        element={
-          <NewNote
-            availableTags={tags}
-            onSubmit={onCreateNote}
-            onAddTag={addTag}
-          />
-        }
-      />
-      <Route path='/task-manager/:id' element={<NoteLayout notes={notesWithTags} />}>
-        <Route index element={<Note onDelete={onDeleteNote} />} />
+        <Route path='/' element={<SpeedType />} />
+        <Route path='/type-speed-game' element={<SpeedType />} />
+        <Route path='/tictoc' element={<TicToc />} />
+        <Route path='/hangman' element={<MainHangMan />} />
+        <Route path='/todo/*' element={<Todo />} />
         <Route
-          path='edit'
+          path='/task-manager'
           element={
-            <EditNote
+            <NoteList
+              notes={notesWithTags}
               availableTags={tags}
-              onSubmit={onUpdateNote}
+              onUpdateTag={onUpdateTag}
+              onDeleteTag={onDeleteTag}
+            />
+          }
+        />
+        <Route
+          path='/task-manager/new'
+          element={
+            <NewNote
+              availableTags={tags}
+              onSubmit={onCreateNote}
               onAddTag={addTag}
             />
           }
         />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>    
+        <Route path='/task-manager/:id' element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route
+            path='edit'
+            element={
+              <EditNote
+                availableTags={tags}
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   )
 }
